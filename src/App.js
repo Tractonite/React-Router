96b6441fa2fe0cfbffb207eddf1e4674507fdd1a
Midhun Padmanabhan A <mid2002hun@gmail.com>
2024-03-07 18:47:05 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
+import Services from './components/Services'
+import Products from './components/Products'
+import CustomerCare from './components/CustomerCare'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Services />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/customer-care' element={<CustomerCare />} />
+    </Routes>
   );
 }
 
---
+++ src/components/CustomerCare.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CustomerCare() {
+  return (
+    <h1>I am Customer Care</h1>
+  )
+}
